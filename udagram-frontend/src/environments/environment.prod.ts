bae385964afb5a34772052989a81d95fbad97fdc
fedeval <federico.valenza5@gmail.com>
2021-06-17 21:03:55 +0300

    chore: add RDS API url to environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "https://udagram.c1gywpg6jwhd.eu-central-1.rds.amazonaws.com/api/v0",
 };
 
 /*
